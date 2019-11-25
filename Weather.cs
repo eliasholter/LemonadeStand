44386f0524c39105476cfc64ae112e6dfbe6d970
Elias Holter <eliasholter1@gmail.com>
2019-11-25 11:26:56 -0600

    Initial set up on Game Constructor, added function to UserInterface to allow user to input player name

---
+++ Weather.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand_3DayStarter
+{
+    class Weather
+    {
+        string condition;
+        int temperature;
+        Random random;
+        List<string> weatherConditions;
+
+        public Weather()
+        {
+            weatherConditions = new List<string>{ "Sunny", "Rainy", "Cloudy", "Snowmaggedon"};
+        }
+
+        public void GenerateWeather()
+        {
+            random = new Random();
+            temperature = random.Next(5, 65);
+            if(temperature <= 32)
+            {
+                condition = weatherConditions[random.Next(0, 4)];
+            }
+            else
+            {
+                condition = weatherConditions[random.Next(0, 3)];
+            }
+        }
+    }
+}
